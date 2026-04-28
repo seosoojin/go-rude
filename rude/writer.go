3046f31b14adce9d2cf710bedd7a0f5fe20cca63
hani <dev.ahn.hani@gmail.com>
2026-04-28 20:48:02 -0300

    refactor: remove over engineering

--- rude/writer.go
+++ rude/writer.go
@@ -2,6 +2,10 @@ package rude
 
 import "net/http"
 
-func WriteProblem(w http.ResponseWriter, r *http.Request, err error) {
+func WriteErrorAsProblem(w http.ResponseWriter, r *http.Request, err error) {
 	FromError(err).Write(w, r)
+}
+
+func WriteProblem(w http.ResponseWriter, r *http.Request, problem ProblemDetails) {
+	problem.Write(w, r)
 }
